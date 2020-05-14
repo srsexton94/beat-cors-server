c95f5f5220e099798c88bb811d591ed8afd2145c
srsexton94 <srsexton94@gmail.com>
2020-05-13 23:01:01 -0400

    Change placeholder url and GET endpoint to rainapp

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get('/jokes/random', (req, res) => {
+app.get('/locations/v1/cities/search', (req, res) => {
   request(
-    { url: 'https://joke-api-strict-cors.appspot.com/jokes/random' },
+    { url: 'http://dataservice.accuweather.com/locations/v1/cities/search' },
     (error, response, body) => {
       if (error || response.statusCode !== 200) {
         return res.status(500).json({ type: 'error', message: err.message });
